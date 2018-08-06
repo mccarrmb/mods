39ffb92d6622480a99a9cb213d3607d0989da96d
mccarrmb <matt.mccarragher@online-rewards.com>
2018-08-06 09:19:22 -0400

    echo -e doesnt work right on macos

--- doom2/mountain/build-pk3.bash
+++ doom2/mountain/build-pk3.bash
@@ -2,13 +2,13 @@
 ARCHIVE_NAME="`basename \`pwd\``.pk3"
 
 if [ -n `command -v zip` ] ; then
-  echo -e "\e[93mBuilding .pk3 archive....\e[0m"
+  printf "\e[93mBuilding .pk3 archive....\e[0m\n"
   zip $ARCHIVE_NAME -v -r . -x $0
   if [ $? -eq 0 ] ; then
-    echo -e "\e[92mArchive $ARCHIVE_NAME successfully built.\e[0m"
+    printf "\e[92mArchive $ARCHIVE_NAME successfully built.\e[0m\n"
   else
-    echo -e "\e[91mArchive build for $ARCHIVE_NAME failed.\e[0m"
+    printf "\e[91mArchive build for $ARCHIVE_NAME failed.\e[0m\n"
   fi
 else
-  echo -e "\e[91mzip utility not found!\e[0m The \".pk3\" format requires zip container/compression." 
+  printf "\e[91mzip utility not found!\e[0m The \".pk3\" format requires zip container/compression.\n" 
 fi
